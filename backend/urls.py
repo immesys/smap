1bc54987d914d1f88f11c28fb29eb8021e69cd85
sdhags <sdhags@511b6515-856b-012f-224d-303ca1f27614>
2011-10-13 21:06:35 +0000

    - patch
    git-svn-id: http://smap-data.googlecode.com/svn/branches/powerdb2@89 511b6515-856b-012f-224d-303ca1f27614

--- backend/urls.py
+++ backend/urls.py
@@ -1,8 +1,6 @@
 
 from django.conf.urls.defaults import *
 
-import powerdb2.backend.views
-
 proxy_conf = {
     'destination' : 'http://local.cs.berkeley.edu:8079',
     'prefix' : 'backend'
--- settings.py
+++ settings.py
@@ -23,15 +23,15 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = 'mysql'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = 'powerdb2'             # Or path to database file if using sqlite3.
-DATABASE_USER = 'powerdb2'             # Not used with sqlite3.
-DATABASE_PASSWORD = 'password'         # Not used with sqlite3.
-DATABASE_HOST = 'www.openbms.org'             # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
-DATABASE_OPTIONS = {
-   "init_command": "SET storage_engine=INNODB",
-}
+# DATABASE_ENGINE = 'mysql'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+# DATABASE_NAME = 'powerdb2'             # Or path to database file if using sqlite3.
+# DATABASE_USER = 'powerdb2'             # Not used with sqlite3.
+# DATABASE_PASSWORD = 'password'         # Not used with sqlite3.
+# DATABASE_HOST = 'www.openbms.org'             # Set to empty string for localhost. Not used with sqlite3.
+# DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
+# DATABASE_OPTIONS = {
+#    "init_command": "SET storage_engine=INNODB",
+# }
 
 # the ARchiver Daemon is a process which manages the archive of your
 # time-series data, and tags.  The Berkeley ARD runs at the location
