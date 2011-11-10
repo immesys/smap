005d2c698033119bdd356db92af1d8042623350c
sdhags <sdhags@511b6515-856b-012f-224d-303ca1f27614>
2011-11-10 00:40:40 +0000

    add support for prefix trees generated from tag values
    git-svn-id: http://smap-data.googlecode.com/svn/branches/powerdb2@98 511b6515-856b-012f-224d-303ca1f27614

--- media/smap/js/tagtree.js
+++ media/smap/js/tagtree.js
@@ -6,26 +6,42 @@ function tag_escape(x){
 function makeTagTree(div, tree_order, selectcb, deselectcb) {
  $(function() {
      var last_selected = [];
+     var separator = '/';
 
      function getTag(ob) {
        if ($.type(ob) == 'string') {
          return ob;
-       } else {
+       } else if ("tag" in ob) {
          return ob.tag;
+       } else if ("prefixTag" in ob) {
+         return ob.prefixTag;
        }
      }
 
-     function buildClauses(p) {
+     function isPrefixTree() {
+       return $.type(tree_order[tree_order.length-1]) == 'object' &&
+         "prefixTag" in tree_order[tree_order.length-1];
+     }
+
+     function buildClauses(p, node) {
         var clauses = [];
-        for (var i = 0; i < p.length; i++) {
+        for (var i = 0; i < Math.min(p.length, tree_order.length); i++) {
           if ($.type(tree_order[i]) == "string") {
             clauses.push(tree_order[i] + " = \"" + tag_escape(p[i]) + '"');
-          } else {
+          } else if ("tag" in tree_order[i]) {
             clauses.push(tree_order[i].tag + " = \"" + tag_escape(p[i]) + '"');
             if ("restrict" in tree_order[i]) {
               clauses.push('(' + tree_order[i].restrict + ')');
             }
           }
+        }
+        if (p.length >= tree_order.length && isPrefixTree()) {
+          // if we're off the end of a prefix tree, condition on the
+          // part of the prefix we've at now.
+          var finalsep = $(node).hasClass("jstree-leaf") ? '' : separator + "%";
+          clauses.push('(' + tree_order[tree_order.length - 1].prefixTag + " like \"" + 
+                       tag_escape(separator + p.slice(tree_order.length-1).join(separator)) + 
+                       finalsep + "\")");
         }
         if (p.length == 0 && $.type(tree_order[0]) == "object" && 
             "restrict" in tree_order[0]) {
@@ -48,22 +64,23 @@ function makeTagTree(div, tree_order, selectcb, deselectcb) {
                ($.type(tree_order[p.length - 1]) == "string" ||
                 ($.type(tree_order[p.length - 1]) == "object" &&
                  !("defaultSubStream" in tree_order[p.length - 1])))) return;
-           new_selection.push(p);
+           new_selection.push([p, this]);
          });
 
        $(last_selected).each(
          function () {
            for (var i = 0; i < new_selection.length; i++) {
-             if (this.toString() == new_selection[i].toString()) {
+             if (this.toString() == new_selection[i][0].toString()) {
                return;
              }
            }
-           deselectcb(this);
+           deselectcb(this[0]);
          });
 
        $(new_selection).each(
          function () {
-           var p = this;
+           var p = this[0];
+           var node = this[1];
            var query = 'select distinct uuid ';
 
            for (var i = 0; i < last_selected.length; i++) {
@@ -71,14 +88,12 @@ function makeTagTree(div, tree_order, selectcb, deselectcb) {
                return;
              }
            }
-           var clauses = " where " + buildClauses(p);
+           var clauses = " where " + buildClauses(p, node);
            if ($.type(tree_order[p.length - 1]) == "object" &&
                "defaultSubStream" in tree_order[p.length - 1]) {
              if (tree_order[p.length - 1].defaultSubStream != '') {
                clauses += " and (" + tree_order[p.length - 1].defaultSubStream + ")";
              }
-           } else if (p.length > tree_order.length) {
-             return;
            }
            console.log(query + clauses);
 
@@ -112,35 +127,75 @@ function makeTagTree(div, tree_order, selectcb, deselectcb) {
                  if (n == -1) {
                    p = [];
                  }
-                 if ($.type(tree_order[p.length]) == "string") {
-                   query += tree_order[p.length];
-                 } else {
-                   query += tree_order[p.length].tag;
+                 if (p.length < tree_order.length ||
+                     (p.length >= tree_order.length && isPrefixTree(tree_order))) {
+                   if ($.type(tree_order[p.length]) == "string") {
+                     query += tree_order[p.length];
+                   } else if (p.length >= tree_order.length - 1 &&
+                             "prefixTag" in tree_order[tree_order.length-1]) {
+                     query += tree_order[tree_order.length-1].prefixTag;
+                   } else if ("tag" in tree_order[p.length]) {
+                     query += tree_order[p.length].tag;
+                   }
+                   var clauses = buildClauses(p, n);
+                   if (clauses.length)
+                     query += " where " + clauses;
+                   console.log(query);
+                   return query;
                  }
-                 var clauses = buildClauses(p);
-                 if (clauses.length)
-                   query += " where " + clauses;
-                 console.log(query);
-                 return query;
                },
                "success" : function (resp, status, req, node) {
                  var obj = eval(resp);
                  var rv = [];
                  var p = $(div).jstree("get_path", node);
                  p = (p == false) ? 0 : p.length;
-                 var sortfn = ($.type(tree_order[p]) == "object" &&
-                               "sortfn" in tree_order[p]) ? tree_order[p].sortfn : undefined;
-                 obj.sort(sortfn);
-                 var state = p == tree_order.length - 1 ? false : "closed";
-                 var icon = (!state || ($.type(tree_order[p]) == "object" &&
-                                        "defaultSubStream" in tree_order[p])) ? "/media/smap/img/plotable.png" :
-                   "/media/smap/img/collection.png";
-                 for (i = 0; i < obj.length; i++) {
-                   rv[i] = {"data" : {
-                              "title": obj[i], 
-                              "icon": icon,
+                 if (p < tree_order.length - 1 || !isPrefixTree()) {
+                   // write tree nodes for the non-prefix-tree portion of the tree
+                   var sortfn = ($.type(tree_order[p]) == "object" &&
+                                 "sortfn" in tree_order[p]) ? tree_order[p].sortfn : undefined;
+                   obj.sort(sortfn);
+                   var state = p == tree_order.length - 1 ? false : "closed";
+                   var icon = (!state || ($.type(tree_order[p]) == "object" &&
+                                          "defaultSubStream" in tree_order[p])) ? 
+                     "/media/smap/img/plotable.png" :
+                     "/media/smap/img/collection.png";
+                   for (i = 0; i < obj.length; i++) {
+                     rv[i] = {"data" : {
+                                "title": obj[i], 
+                                "icon": icon,
                               },
-                            "state": state};
+                              "state": state};
+                   }
+                 } else if (isPrefixTree()) {
+                   // build a prefix tree 
+                   var level = p - tree_order.length + 1 + 1;
+                   var tags = {};
+                   var sortfn = ($.type(tree_order[tree_order.length - 1]) == "object" &&
+                                 "sortfn" in tree_order[tree_order.length - 1]) ? 
+                     tree_order[tree_order.length - 1].sortfn : undefined;
+
+                   // we're in a prefix tree... 
+                   // find the distinct tags, and decide if they are open or closed
+                   for (i = 0; i < obj.length; i++) {
+                     var cmps = obj[i].split(separator);
+                     if (cmps.length >= level) {
+                       var thisstate = cmps.length > level + 1 ? "closed" : false;
+                       // console.log("checking " + cmps[level] + " " + tags[cmps[level]]);
+                       if (tags[cmps[level]] == undefined ||
+                           tags[cmps[level]] == false) {
+                         tags[cmps[level]] = thisstate;
+                       }
+                     }
+                   }
+
+                   // write back the consolidated tag values
+                   for (var tv in tags) {
+                     rv[rv.length] = {"data" : {
+                                        "title" : tv,
+                                        "icon" : "/media/smap/img/plotable.png",
+                                      },
+                                      "state": tags[tv]};
+                   }
                  }
                  return rv;
                }
